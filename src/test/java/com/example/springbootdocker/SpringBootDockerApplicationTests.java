1c1b3bed5a46c58704a6427c6cd1a865a5bcb647
Andie Cheung <88743961+ancheung01@users.noreply.github.com>
2022-06-01 14:51:17 -0700

    chore(junit + html): correct html text + setup junit

--- src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
+++ src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
@@ -1,10 +1,19 @@
 package com.example.springbootdocker;
 
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootDockerApplicationTests {
+
+	@BeforeAll
+	static void setup() {
+		Security.addProvider(new BouncyCastleProvider());
+	}
 
 	@Test
 	void contextLoads() {
